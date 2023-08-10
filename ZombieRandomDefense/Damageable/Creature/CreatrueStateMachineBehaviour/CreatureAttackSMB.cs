fe7af0e4becbbc77563fc0b832741e3828a3b785
Kyo <kyosun97@gmail.com>
2023-08-10 16:18:52 +0900

    Removed generic type

--- ZombieRandomDefense/Damageable/Creature/CreatrueStateMachineBehaviour/CreatureAttackSMB.cs
+++ ZombieRandomDefense/Damageable/Creature/CreatrueStateMachineBehaviour/CreatureAttackSMB.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CreatureAttackSMB : CreatureStateMachineBehaviour<Creature>
+public class CreatureAttackSMB : CreatureStateMachineBehaviour
 {
     
 }
